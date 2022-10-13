83e61b4bf1c7e81182dc30fa13a26c1ab911138a
Marie-Claire Fayose <nfayose@gmail.com>
2022-10-13 12:07:07 -0500

    Walked through melons.py

--- melons.py
+++ melons.py
@@ -30,8 +30,10 @@ class Melon:
 
     def price_str(self):
         """Return price formatted as string $x.xx"""
-
+         
         return f"${self.price:.2f}"
+
+        
 
     def __repr__(self):
         """Convenience method to show information about melon in console."""
